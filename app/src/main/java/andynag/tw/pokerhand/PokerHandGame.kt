f90d5a3f2a050296e387b77655addf5ad33ab328
andyang <yang.bng86@gmail.com>
2019-01-14 01:21:24 +0800

    test draw pass

--- app/src/main/java/andynag/tw/pokerhand/PokerHandGame.kt
+++ app/src/main/java/andynag/tw/pokerhand/PokerHandGame.kt
@@ -4,16 +4,23 @@ class PokerHandGame(private val player1: Player, private val player2: Player) {
 
     fun getWinner(): Winner {
         return when {
-            player1.hands > player2.hands -> Winner(player1.name, player1.hands.getHandCategory())
-            player1.hands < player2.hands -> Winner(player2.name, player2.hands.getHandCategory())
-            else -> TODO("not implement")
+            player1.hands > player2.hands -> Winner.Win(player1.name, player1.hands.getHandCategory())
+            player1.hands < player2.hands -> Winner.Win(player2.name, player2.hands.getHandCategory())
+            else -> Winner.Draw(player1.hands.getHandCategory())
         }
     }
 }
 
-data class Winner(val name: String, val handCategory: HandCategory) {
+sealed class Winner {
+    data class Win(val name: String, val handCategory: HandCategory) : Winner() {
+        override fun toString(): String {
+            return "$name win, because of $handCategory"
+        }
+    }
 
-    override fun toString(): String {
-        return "$name win, because of $handCategory"
+    data class Draw(val handCategory: HandCategory) : Winner() {
+        override fun toString(): String {
+            return "Draw, because of $handCategory"
+        }
     }
 }
