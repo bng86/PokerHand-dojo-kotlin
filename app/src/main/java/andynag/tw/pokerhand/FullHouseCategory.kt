4028ff12e6e77ed73166c40db50c1026c964e21b
andyang <yang.bng86@gmail.com>
2019-01-13 21:56:46 +0800

    test hands get hands category full house pass

---
+++ app/src/main/java/andynag/tw/pokerhand/FullHouseCategory.kt
@@ -0,0 +1,14 @@
+package andynag.tw.pokerhand
+
+class FullHouseCategory : HandsCategoryMatcher {
+
+    override fun isMatch(cards: List<Card>): Boolean {
+        val groupPoint = cards.groupBy { it.getPoints() }
+        return groupPoint.size == 2 && groupPoint.any { it.value.size == 3 }
+    }
+
+    override fun getHandsCategory(): HandCategory {
+        return HandCategory.FullHouse
+    }
+
+}
--- app/src/main/java/andynag/tw/pokerhand/Hands.kt
+++ app/src/main/java/andynag/tw/pokerhand/Hands.kt
@@ -4,11 +4,12 @@ class Hands {
 
     private val matchers = listOf(
         StraightFlushMatcher(),
-        FourOfAKindMatcher()
+        FourOfAKindMatcher(),
+        FullHouseCategory()
     )
 
     fun getHandCategory(cards: List<Card>): HandCategory {
         val handsCategory: HandCategory? = matchers.firstOrNull { it.isMatch(cards) }?.getHandsCategory()
-        return handsCategory ?: throw Exception("known hands category exception")
+        return handsCategory ?: throw Throwable("known hands category exception")
     }
 }
