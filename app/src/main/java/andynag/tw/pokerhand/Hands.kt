cdce8616e4de68ec7fd01788a3bfacdbdc9cf5bb
andyang <yang.bng86@gmail.com>
2019-01-14 00:29:53 +0800

    test build player pass

--- app/src/main/java/andynag/tw/pokerhand/Hands.kt
+++ app/src/main/java/andynag/tw/pokerhand/Hands.kt
@@ -19,4 +19,20 @@ class Hands(val cards: List<Card>) {
         val handsCategory: HandCategory? = matchers.firstOrNull { it.isMatch(this.cards) }?.getHandsCategory()
         return handsCategory ?: HandCategory.HighCard
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Hands
+
+        if (cards != other.cards) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return cards.hashCode()
+    }
+
 }
