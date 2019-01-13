34890258c9039c045ae36ac3148cae8e3d403386
andyang <yang.bng86@gmail.com>
2019-01-14 00:59:08 +0800

    refactor poker hand game test case

--- app/src/test/java/andynag/tw/pokerhand/PokerHandGameTest.kt
+++ app/src/test/java/andynag/tw/pokerhand/PokerHandGameTest.kt
@@ -5,26 +5,31 @@ import org.junit.Test
 
 class PokerHandGameTest {
 
-    @Test
-    fun test_player1_andy_straight_flush_win_player2_merry_four_of_a_kind() {
-        val cards1 = listOf(
+    private val straightFlushHands = Hands(
+        listOf(
             Card("CA"),
             Card("C2"),
             Card("C3"),
             Card("C4"),
             Card("C5")
         )
+    )
 
-        val cards2 = listOf(
+    private val fourOfAKindHands = Hands(
+        listOf(
             Card("CA"),
             Card("DA"),
             Card("HA"),
             Card("SA"),
             Card("C5")
         )
+    )
 
-        val player1 = Player("Andy", Hands(cards1))
-        val player2 = Player("Merry", Hands(cards2))
+    @Test
+    fun test_player1_andy_straight_flush_win_player2_merry_four_of_a_kind() {
+
+        val player1 = Player("Andy", straightFlushHands)
+        val player2 = Player("Merry", fourOfAKindHands)
 
         val pokerHandGame = PokerHandGame(player1, player2)
         val winner = pokerHandGame.getWinner()
