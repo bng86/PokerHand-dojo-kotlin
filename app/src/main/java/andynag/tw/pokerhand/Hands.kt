85c9a088601cbf5229cf3ff7624612786e1556f3
andyang <yang.bng86@gmail.com>
2019-01-13 20:38:42 +0800

    test hands get hands category straight flush pass

--- app/src/main/java/andynag/tw/pokerhand/Hands.kt
+++ app/src/main/java/andynag/tw/pokerhand/Hands.kt
@@ -2,6 +2,19 @@ package andynag.tw.pokerhand
 
 class Hands(val cards: List<Card>) {
     fun getHandCategory(): HandCategory {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val allDifference = cards.distinctBy { it.getPoints() }.count() == 5
+        val isStraight = if (allDifference) {
+            val max = cards.map { it.getPoints() }.max()!!
+            val min = cards.map { it.getPoints() }.min()!!
+            max - min == 4
+        } else {
+            false
+        }
+        val isFlush = cards.distinctBy { it.getSuit() }.count() == 1
+        if (isStraight && isFlush) {
+            return HandCategory.StraightFlush
+        } else {
+            TODO("not implement")
+        }
     }
 }
