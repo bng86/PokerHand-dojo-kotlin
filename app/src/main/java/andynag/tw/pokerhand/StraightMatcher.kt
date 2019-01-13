bd93edeb3bf3ef869e64331ac9427e3b890f9291
andyang <yang.bng86@gmail.com>
2019-01-13 22:30:19 +0800

    test hands get hands category straight 10JQKA pass

--- app/src/main/java/andynag/tw/pokerhand/StraightMatcher.kt
+++ app/src/main/java/andynag/tw/pokerhand/StraightMatcher.kt
@@ -5,13 +5,22 @@ class StraightMatcher : HandsCategoryMatcher {
     override fun isMatch(cards: List<Card>): Boolean {
         val allDifference = cards.distinctBy { it.getPoints() }.count() == 5
         return if (allDifference) {
-            val max = cards.map { it.getPoints() }.max()!!
-            val min = cards.map { it.getPoints() }.min()!!
-            max - min == 4
+            if (hasA(cards) && is10JQKA(cards)) {
+                return true
+            } else {
+                val max = cards.map { it.getPoints() }.max()!!
+                val min = cards.map { it.getPoints() }.min()!!
+                max - min == 4
+            }
         } else {
             false
         }
     }
+
+    private fun is10JQKA(cards: List<Card>) =
+        cards.map { it.getPoints() }.sortedBy { it }.toList() == listOf(1, 10, 11, 12, 13)
+
+    private fun hasA(cards: List<Card>) = cards.any { it.getPoints() == 1 }
 
     override fun getHandsCategory(): HandCategory {
         return HandCategory.Straight
