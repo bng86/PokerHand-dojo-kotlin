ae6657ebf67a71d2f921e58930bddedb900e26a3
andyang <yang.bng86@gmail.com>
2019-01-13 19:55:37 +0800

    test diamonds Q convert to card pass

--- app/src/main/java/andynag/tw/pokerhand/Card.kt
+++ app/src/main/java/andynag/tw/pokerhand/Card.kt
@@ -1,11 +1,22 @@
 package andynag.tw.pokerhand
 
-class Card(inputString: String) {
+class Card(private val inputString: String) {
     fun getSuit(): SuitType {
-        return SuitType.Clubs
+        val suitString = inputString.substring(0, 1)
+        return when (suitString) {
+            "C" -> SuitType.Clubs
+            "D" -> SuitType.Diamonds
+            else -> TODO("unknown suit type")
+        }
     }
 
     fun getPoints(): Int {
-        return 11
+        val pointsString = inputString.substring(1)
+        return when (pointsString) {
+            "J" -> 11
+            "Q" -> 12
+            else -> TODO("unknown points")
+        }
+
     }
 }
