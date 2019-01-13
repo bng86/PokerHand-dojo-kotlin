6df4f0756070cfe42d088fb51d67b5ad67eec387
andyang <yang.bng86@gmail.com>
2019-01-13 19:11:46 +0800

    first commit

---
+++ app/src/main/java/andynag/tw/pokerhand/MainActivity.kt
@@ -0,0 +1,12 @@
+package andynag.tw.pokerhand
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
