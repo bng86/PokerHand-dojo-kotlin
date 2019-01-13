b9b2e1e6fee7824580b933acb83af8811407df02
andyang <yang.bng86@gmail.com>
2019-01-13 20:49:24 +0800

    test hands get hands category straight flush refactoring extract method

--- app/src/main/java/andynag/tw/pokerhand/Hands.kt
+++ app/src/main/java/andynag/tw/pokerhand/Hands.kt
@@ -1,20 +1,27 @@
 package andynag.tw.pokerhand
 
 class Hands(val cards: List<Card>) {
+
     fun getHandCategory(): HandCategory {
+        if (isStraightFlush(cards)) {
+            return HandCategory.StraightFlush
+        } else {
+            TODO("not implement")
+        }
+    }
+
+    private fun isStraightFlush(cards: List<Card>) = isStraight(cards) && isFlush(cards)
+
+    private fun isFlush(cards: List<Card>) = cards.distinctBy { it.getSuit() }.count() == 1
+
+    private fun isStraight(cards: List<Card>): Boolean {
         val allDifference = cards.distinctBy { it.getPoints() }.count() == 5
-        val isStraight = if (allDifference) {
+        return if (allDifference) {
             val max = cards.map { it.getPoints() }.max()!!
             val min = cards.map { it.getPoints() }.min()!!
             max - min == 4
         } else {
             false
-        }
-        val isFlush = cards.distinctBy { it.getSuit() }.count() == 1
-        if (isStraight && isFlush) {
-            return HandCategory.StraightFlush
-        } else {
-            TODO("not implement")
         }
     }
 }
