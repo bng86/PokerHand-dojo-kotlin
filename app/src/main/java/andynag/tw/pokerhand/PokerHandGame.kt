c3d1b6edd99003feaf4bdcecb4e72494754d08a7
andyang <yang.bng86@gmail.com>
2019-01-14 00:42:54 +0800

    test straight flush win four of a kind fail

---
+++ app/src/main/java/andynag/tw/pokerhand/PokerHandGame.kt
@@ -0,0 +1,11 @@
+package andynag.tw.pokerhand
+
+class PokerHandGame(player1: Player, player2: Player) {
+    fun getWinner(): Winner {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class Winner {
+
+}
---
+++ app/src/test/java/andynag/tw/pokerhand/PokerHandGameTest.kt
@@ -0,0 +1,34 @@
+package andynag.tw.pokerhand
+
+import org.junit.Assert
+import org.junit.Test
+
+class PokerHandGameTest {
+
+    @Test
+    fun test_straight_flush_win_four_of_a_kind() {
+        val cards1 = listOf(
+            Card("CA"),
+            Card("C2"),
+            Card("C3"),
+            Card("C4"),
+            Card("C5")
+        )
+
+        val cards2 = listOf(
+            Card("CA"),
+            Card("DA"),
+            Card("HA"),
+            Card("SA"),
+            Card("C5")
+        )
+
+        val player1 = Player("Andy", Hands(cards1))
+        val player2 = Player("Merry", Hands(cards2))
+
+        val pokerHandGame = PokerHandGame(player1, player2)
+        val winner = pokerHandGame.getWinner()
+
+        Assert.assertEquals("Andy win, because of Straight Flush", winner.toString())
+    }
+}
