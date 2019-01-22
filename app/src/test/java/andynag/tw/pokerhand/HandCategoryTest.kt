2040d290d92552f8507bfd20e0db1faf7e29d76d
andyang <yang.bng86@gmail.com>
2019-01-22 19:23:22 +0800

    redefined hands category test case

--- app/src/test/java/andynag/tw/pokerhand/HandCategoryTest.kt
+++ app/src/test/java/andynag/tw/pokerhand/HandCategoryTest.kt
@@ -1,20 +1,38 @@
 package andynag.tw.pokerhand
 
+import andynag.tw.pokerhand.HandCategory.*
 import org.junit.Assert
 import org.junit.Test
 
 class HandCategoryTest {
 
     @Test
-    fun test_hands_category_ranking_mapping() {
-        Assert.assertEquals(HandCategory.StraightFlush.value, 9)
-        Assert.assertEquals(HandCategory.FourOfAKind.value, 8)
-        Assert.assertEquals(HandCategory.FullHouse.value, 7)
-        Assert.assertEquals(HandCategory.Flush.value, 6)
-        Assert.assertEquals(HandCategory.Straight.value, 5)
-        Assert.assertEquals(HandCategory.ThreeOfAKind.value, 4)
-        Assert.assertEquals(HandCategory.TwoPair.value, 3)
-        Assert.assertEquals(HandCategory.OnePair.value, 2)
-        Assert.assertEquals(HandCategory.HighCard.value, 1)
+    fun test_hands_category_ranking_priority() {
+
+        val expected = listOf(
+            StraightFlush,
+            FourOfAKind,
+            FullHouse,
+            Flush,
+            Straight,
+            ThreeOfAKind,
+            TwoPair,
+            OnePair,
+            HighCard
+        )
+
+        val actual = listOf(
+            HighCard,
+            OnePair,
+            TwoPair,
+            ThreeOfAKind,
+            Straight,
+            Flush,
+            FullHouse,
+            FourOfAKind,
+            StraightFlush
+        ).sortedByDescending { it.value }
+
+        Assert.assertEquals(expected, actual)
     }
 }
